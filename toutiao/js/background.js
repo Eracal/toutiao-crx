ca753be19ca3d85dae1f6cc2a786c4cde01f9cb2
Eriice <694979377@qq.com>
2019-08-09 17:34:44 +0800

    新增是否在运行中的状态提示

--- toutiao/js/background.js
+++ toutiao/js/background.js
@@ -1,12 +1,30 @@
+let count = 0
 // 监听来自content-script的消息
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 {
+  // status: run启动定时器 | err发生错误 | well状态变更 | stop退出页面
+  if (request.status === 'well' || request.status === 'err') {
+    chrome.notifications.create(null, {
+      type: 'basic',
+      iconUrl: 'img/logo.png',
+      title: '审查状态：',
+      message: request.msg,
+      requireInteraction: true
+    });
+  }
 
-  chrome.notifications.create(null, {
-    type: 'basic',
-    iconUrl: 'img/logo.png',
-    title: '审查状态：',
-    message: request.msg,
-    requireInteraction: true
-  });
+
+  chrome.browserAction.setBadgeBackgroundColor({color: '#1ab21e'});
+  if (request.status === 'run') {
+    count++
+    chrome.browserAction.setBadgeText({text: request.status});
+  } else if (request.status === 'stop') {
+    // 如果退出页面且计数器为 0， 则证明已经不存在任何检测页面，状态变更为 stop
+    count--
+    if (count === 0) {
+      chrome.browserAction.setBadgeText({text: request.status});
+    }
+  }
+
+  
 });
--- toutiao/js/watch.js
+++ toutiao/js/watch.js
@@ -1,37 +1,57 @@
 $(document).ready(function () {
+  
+  chrome.runtime.sendMessage({
+    msg: `开始运行`,
+    status: 'run'
+  });
+
+  // 检查是否状态变更
+  checkStatus()
+
+  setInterval(() => {
+    // 刷新
+    window.location.reload()
+  }, 20*60*1000)
+
+});
+
+$(window).on("unload", function(e) {
+  chrome.runtime.sendMessage({
+    msg: ` 退出页面`,
+    status: 'stop'
+  });
+});
+
+
+function checkStatus() {
   let time = 0
   let oTimer = setInterval(() => {
 
     let tagNode = $('.audit-state-tag')
     if (tagNode.length !== 0) {
       let text= tagNode.eq(0).text()
-      chrome.runtime.sendMessage({
-        msg: `开始检测审核状态是否变更，当前状态为：${text} `
-      });
+      sendNotification(text)
       clearInterval(oTimer)
     }
 
+    // 防止头条后台更新后找不到节点
     time++
-    if (time > 20) {
+    if (time > 10) {
       chrome.runtime.sendMessage({
-        msg: `找不到检测节点，到 Github 查看插件新版本`
+        msg: `找不到检测节点，到 Github 查看插件新版本`,
+        status: `err`
       });
       clearInterval(oTimer)
     }
-  }, 500)
+  }, 1000)
+}
 
-  setInterval(() => {
-    checkStatus()
-    window.location.reload()
-  }, 20*60*1000)
-});
-
-function checkStatus() {
-  let tag = $('.audit-state-tag').eq(0)
-  if (tag.text() !== '审核中') {
+function sendNotification(text) {
+  if (text !== '审核中') {
     // 创建一个简单的文本通知：
     chrome.runtime.sendMessage({
-      msg: `当前状态为：${tag.text()}`
+      msg: `当前状态为：${text}`,
+      status: `well`
     }, function (response) {
     });
   }
